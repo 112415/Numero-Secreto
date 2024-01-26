a0a5c1eda34f492338ad5a8c314b81e5f5de63b2
112415 <6452158+github@gmail.com>
2024-01-25 19:31:03 -0600

    JS terminado con while

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ titulo.innerHTML = 'Juego del número secreto';
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Indica un número del 1 al 10';
 
-function intentoDeUsuario(){
-    alert('Click desde el botón');
+let numeroMin = parseInt(prompt(`Ingrese número mínimo: `));
+let numeroMax = parseInt(prompt(`Ingrese número máximo: `));
+let numeroSecreto = Math.trunc(Math.random()*numeroMax)+numeroMin;
+console.log(numeroSecreto);
+let numeroUsuario = 0;
+let contador = 0;
+let limiteIntentos = 3;
+
+while(numeroUsuario != numeroSecreto){
+    contador++;
+    numeroUsuario = parseInt(prompt(`Ingrese número para adivinar: `));
+    if(numeroUsuario > numeroSecreto){
+        alert('El número secreto es menor.');
+    }else if(numeroUsuario < numeroSecreto){
+        alert('El número secreto es mayor.');
+    }else{
+        alert(`Acertaste. Número secreto es: ${numeroSecreto}. Lo lograste en ${contador} ${contador == 1 ? 'intento' : 'intentos'}`);
+        break;
+    }
+    if(contador == limiteIntentos){
+        alert(`Haz logrado el máximo de ${contador} ${contador == 1 ? 'intento' : 'intentos'} posibles.`)
+        break;
+    }
 }
