5bd5e536cffbf476218c9ae76387a78d46512d05
112415 <6452158+github@gmail.com>
2024-01-26 10:57:34 -0600

    Añadido el reinicio del juego

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
-console.log(`El número sercreto es: ${numeroSecreto}`);
+let numeroSecreto = 0;
+let intentos = 0;
+//console.log(`El número sercreto es: ${numeroSecreto}`);
+
+function condicionesIniciales() {
+    //Resetear texto inicial
+    asignarTextoElemento('h1', 'Juego del número secreto');
+    asignarTextoElemento('p', 'Indica un número del 1 al 10');
+    //Resetear numero secreto
+    numeroSecreto = generarNumeroSecreto();
+    console.log(`El número sercreto es: ${numeroSecreto}`);
+    //Resetear intentos
+    intentos = 1;
+    //Limpiar caja input
+    limpiarCaja();
+    //deshabilitar boton nuevo juego
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
+
+function reinciarJuego() {
+    condicionesIniciales();
+}
 
 function generarNumeroSecreto() {
     return Math.trunc(Math.random()*10)+1;    
@@ -34,5 +53,4 @@ function verificarIntento() {
     return;
 }
 
-asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Indica un número del 1 al 10');
+condicionesIniciales();
