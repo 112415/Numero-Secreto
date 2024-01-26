a9720a807386cd8c355aed7c1b8fd975caf42158
112415 <6452158+github@gmail.com>
2024-01-25 21:55:43 -0600

    Se agrega functions e inputs

--- app.js
+++ app.js
@@ -1,31 +1,25 @@
-
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
-
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
-
-let numeroMin = parseInt(prompt(`Ingrese número mínimo: `));
-let numeroMax = parseInt(prompt(`Ingrese número máximo: `));
-let numeroSecreto = Math.trunc(Math.random()*numeroMax)+numeroMin;
+let numeroSecreto = generarNumeroSecreto();
 console.log(numeroSecreto);
-let numeroUsuario = 0;
-let contador = 0;
-let limiteIntentos = 3;
 
-while(numeroUsuario != numeroSecreto){
-    contador++;
-    numeroUsuario = parseInt(prompt(`Ingrese número para adivinar: `));
-    if(numeroUsuario > numeroSecreto){
-        alert('El número secreto es menor.');
-    }else if(numeroUsuario < numeroSecreto){
-        alert('El número secreto es mayor.');
-    }else{
-        alert(`Acertaste. Número secreto es: ${numeroSecreto}. Lo lograste en ${contador} ${contador == 1 ? 'intento' : 'intentos'}`);
-        break;
-    }
-    if(contador == limiteIntentos){
-        alert(`Haz logrado el máximo de ${contador} ${contador == 1 ? 'intento' : 'intentos'} posibles.`)
-        break;
-    }
+function generarNumeroSecreto() {
+    return Math.trunc(Math.random()*10)+1;    
 }
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;    
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(`El tipo de dato para numeroDeUsuario es: ${typeof(numeroDeUsuario)}`);
+    if (numeroDeUsuario === numeroSecreto) {
+        console.log(`El tipo de dato comparado es: ${typeof(numeroDeUsuario === numeroSecreto)}`);
+        alert(`Acertaste. El número secreto es: ${numeroSecreto}`);
+    }
+    return;
+}
+
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Indica un número del 1 al 10');
